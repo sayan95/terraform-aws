26c96d864842be86286ee1589d73cf2f763bf984
sharthak123 <sayanmallik1995@gmail.com>
2021-02-21 23:30:35 +0530

    EC2 Ubuntu instace created

---
+++ main.tf
@@ -0,0 +1,12 @@
+// provider
+provider "aws" {
+    region = "ap-south-1"
+    access_key = "AKIAW75X2FLBVCVIXTNZ"
+    secret_key = "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"
+}
+
+// create a virtual network (vpc)
+resource "aws_instance" "company-EC2Ubuntu" {
+  ami = "ami-073c8c0760395aab8"
+  instance_type = "t2.micro"
+}
