1d50dec5fd67f745475825cf97315aa94e133b65
sharthak123 <sayanmallik1995@gmail.com>
2021-02-21 23:39:37 +0530

    adding required procider in main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws={
+      source = "hashicorp/aws"
+      version = "~>3.29.0"
+    }
+  }
+}
+
 // provider
 provider "aws" {
     region = "ap-south-1"
